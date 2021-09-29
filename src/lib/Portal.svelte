cd22c35af672e7dc061af193e11f9143dfa6ab3d
renato.perez <renatoperezc@gmail.com>
2021-09-29 12:03:40 -0600

    Add Portal component to render its content as direct children of body

---
+++ src/lib/Portal.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+
+	let ref: HTMLDivElement;
+	// let portal: HTMLDivElement;
+
+	// onMount(() => {
+	// 	portal = document.createElement('div');
+	// 	portal.className = 'portal';
+	// 	document.body.appendChild(portal);
+	// 	portal.appendChild(ref);
+	// 	return () => document.body.removeChild(portal);
+	// });
+	// onDestroy(() => {
+	// 	document.body.removeChild(portal);
+	// });
+	function portal(node: HTMLDivElement) {
+		let target: HTMLBodyElement | HTMLElement;
+		console.log({ node });
+		function update() {
+			document.body.append(node);
+			node.hidden = false;
+		}
+		function destroy() {
+			if (node.parentNode) {
+				node.parentNode.removeChild(node);
+			}
+		}
+		update();
+		return {
+			update,
+			destroy
+		};
+	}
+</script>
+
+<!-- <div class="portal-clone">
+	<div bind:this={ref}>
+		<slot />
+	</div>
+</div> -->
+<div use:portal hidden>
+	<slot />
+</div>
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import Markdown from '$lib/Markdown.svelte';
-
-	let items = ['Samson', 'scott', 'wes', 'landon', 'courtney', 'lucie', 'brooklyn'];
-	let search = '';
+	import Portal from '$lib/Portal.svelte';
 </script>
 
 <div style="width: 100%; padding:0.5rem 0.25rem;">
 	<h1>Welcome to Mayan UI</h1>
-	<Markdown />
 </div>
+<Portal>
+	<input type="text" />
+</Portal>
