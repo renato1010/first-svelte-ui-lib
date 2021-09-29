2abf4e08646e6552b71688c6fa258a90d92e651b
renato.perez <renatoperezc@gmail.com>
2021-09-28 18:24:59 -0600

    End Field component

---
+++ src/lib/Field.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let label = '';
+	export let instructions = '';
+	export let style = '';
+	export let value: string | number;
+	export let placeholder: string;
+	export let type: 'text' | 'email' | 'number' | 'password';
+
+	function handleInput(
+		e: Event & {
+			currentTarget: EventTarget & HTMLInputElement;
+		}
+	) {
+		value = type.match(/^(number|range)$/) ? +e.currentTarget.value : e.currentTarget.value;
+	}
+</script>
+
+<div {style}>
+	<label>
+		{#if label}
+			<span>{label}</span>
+		{/if}
+		{#if instructions}
+			<span class="instructions">{instructions}</span> <br />
+		{/if}
+		<input on:input={handleInput} {type} {value} {placeholder} />
+	</label>
+</div>
+
+<style>
+	.instructions {
+		opacity: 0.7;
+		font-size: var(--instructionsFontSize, 0.8rem);
+	}
+	input {
+		height: 2.5rem;
+		width: 40ch;
+		margin: 0.5rem 0;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import SearchFilter from '$lib/SearchFilter.svelte';
+	import Field from '$lib/Field.svelte';
 
 	let items = ['Samson', 'scott', 'wes', 'landon', 'courtney', 'lucie', 'brooklyn'];
 	let search = '';
@@ -7,7 +7,11 @@
 
 <div style="width: 100%; padding:0.5rem 0.25rem;">
 	<h1>Welcome to Mayan UI</h1>
-	<p>Term: {search}</p>
-
-	<SearchFilter {items} bind:search />
+	<Field
+		bind:value={search}
+		label="search"
+		instructions="Type to search"
+		placeholder="Joe"
+		type="text"
+	/>
 </div>
